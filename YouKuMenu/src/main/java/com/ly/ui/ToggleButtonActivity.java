3a324fd60786d2425cf4ed8dd668b8fba0b03817
ly <986417576@qq.com>
2015-02-05 17:32:11 +0800

    添加了下拉菜单和广告条功能

---
+++ YouKuMenu/src/main/java/com/ly/ui/ToggleButtonActivity.java
@@ -0,0 +1,17 @@
+package com.ly.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ly.ui.R;
+
+public class ToggleButtonActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toggle_button);
+    }
+
+
+}
