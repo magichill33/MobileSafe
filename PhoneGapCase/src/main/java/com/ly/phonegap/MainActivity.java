8223ce52251968939bd5841a98a9cac74d8c6429
ly <986417576@qq.com>
2015-03-23 16:38:48 +0800

    一个phoneGap示例

--- PhoneGapCase/src/main/java/com/ly/phonegap/MainActivity.java
+++ PhoneGapCase/src/main/java/com/ly/phonegap/MainActivity.java
@@ -1,39 +1,21 @@
 package com.ly.phonegap;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.apache.cordova.DroidGap;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends DroidGap {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl("file:///android_asset/www/index.html");
+        //super.loadUrl("file:///android_asset/www/index.html")
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
